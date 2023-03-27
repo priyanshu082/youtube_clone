bc109850ad51a651d852cf54155db5bb3fdbb248
priyanshu082 <priyanshusingh216@gmail.com>
2023-03-27 11:15:09 +0530

    Feed is complete ,now channle detail will start

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { Box,CardContent,CardMedia,Typography } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
-import { display, height } from '@mui/system'
+
 
 
 export const ChannelCard = ({channelDetail}) => {
